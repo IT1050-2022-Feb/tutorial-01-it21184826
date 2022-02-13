45896deb9544862e03ea0e28ac3fd425a526f84e
it21184826 <99355177+it21184826@users.noreply.github.com>
2022-02-13 17:12:18 +0000

    Have done all

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int mark,i;
+  float avg,sum;
+
+  for (i=0;i<2;++i)
+  {
+    printf("Enter the marks of subject %d :",i+1);
+    scanf("%d",&mark);
+    sum=sum+mark;
+  }
+  avg=sum/2.0;
+  printf("Your average marks is %.2f",avg);
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,20 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  int distance;
+  float amount;
+
+  printf("Enter the distance km : ");
+  scanf("%d",&distance);
+
+  if(distance<=30)
+  {
+    amount=distance*50.00;
+  }
+  else
+  {
+    amount=(30.00*50.00) +( (distance-30)*40.00);
+  }
+  printf("Your amount is Rs. : %.2f",amount);
   return 0;
 }
